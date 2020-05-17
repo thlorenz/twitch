2dbffe2a11aa8f16745c32acd9559bb781048d39
Thorsten Lorenz <thlorenz@gmx.de>
2020-05-16 19:32:58 -0600

    secrets: adding test secrets to source during development

---
+++ tools/test-secret.sh
@@ -0,0 +1,4 @@
+export TWITCH_OAUTH_ID='twitch oauth id'
+export TWITCH_CLIENT_ID='twitch client id'
+export TWITCH_REFRESH_TOKEN='twitch refresh token'
+export TWITCH_CLIENT_SECRET='twitch client secret'
