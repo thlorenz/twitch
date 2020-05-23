761b11ff54c4f83f06c61cd23f2385dc8d9d7bc4
Thorsten Lorenz <thlorenz@gmx.de>
2020-05-22 22:50:35 -0600

    edit-stream: update status working as long as no new lines

--- tools/edit-stream-info/edit-stream-info.el
+++ tools/edit-stream-info/edit-stream-info.el
@@ -6,9 +6,10 @@
 (require 'org)
 (require 'org-element)
 (require 'cl-macs)
+(require 'request)
 
-(defun twitch--edit-stream-first-header+content ()
-  "Get first header and it's content from a buffer."
+(defun first--header-and-content ()
+  "Get first header and it's content current buffer."
   (let ((bb))
     (block bb
       (org-element-map (org-element-parse-buffer) 'headline
@@ -20,20 +21,53 @@
                (org-element-property :raw-value headline)
                (buffer-substring contents-begin (- contents-end 1))))))))))
 
-(defun twitch--edit-stream-first-header+content-buffer ()
+(defun first-header-and-content-buffer ()
   "Get first header and it's content from current buffer."
   (interactive)
-  (with-current-buffer (current-buffer) (twitch--edit-stream-first-header+content)))
+  (with-current-buffer
+      (current-buffer)
+    (first-header-and-content)))
+
+
+
+(defun twitch--update-status (channel-id status)
+  "Update twitch channel CHANNEL-ID with STATUS."
+  (let ((client-id (getenv "TWITCH_CLIENT_ID"))
+        (oauth-id (getenv "TWITCH_OAUTH_ID")))
+    (custom-request
+      (format "https://api.twitch.tv/kraken/channels/%d" channel-id)
+      :sync t
+      :type "PUT"
+      :headers `(("Client-ID" . ,(format "%s" client-id))
+                 ("Authorization" . ,(format "OAuth %s" oauth-id))
+                 ("Accept" . "application/vnd.twitchtv.v5+json")
+                 ("Content-Type" . "application/json"))
+      :parser 'json-read
+      :data (format "{\"channel\": {\"status\": \"%s\", \"game\": \"Science & Technology\", \"channel_feed_enabled\": true}}" status)
+      :success (cl-function
+                (lambda (&key data &allow-other-keys)
+                  (message "I sent: %S" (assoc-default 'headers data))))
+      :error
+      (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
+                     (pp error-thrown))))))
+
+(defun twitch-update ()
+  "Update twitch channel status with first header of current org file."
+  (interactive)
+  (let ((status (nth 1 (first--header-and-content)))
+        (channel-id 145377094))
+    (twitch--update-status channel-id status)))
+
+(twitch--update-status  145377094 "Goin on three seconds.")
 
 ;; TESTING
 (with-temp-buffer
   (insert "
 * Hacking Emacs Lisp
 
-Connecting emacs, elisp, org-mode and twitch.
-Learning how to parse org mode.
+Connecting emacs, elisp, org-mode and twitch. Learning how to parse org mode.
 ")
-  (twitch--edit-stream-first-header+content))
+  (twitch-update))
 
 (provide 'edit-stream-info)
 
